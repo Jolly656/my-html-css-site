942991e18f47cf70ad292694d7ad7aa9111b1613
Jolly656 <1031638176@qq.com>
2025-06-09 20:29:38 +0800

    Update site content

--- push_update.bat
+++ push_update.bat
@@ -2,13 +2,13 @@
 REM 设置当前工作目录
 cd /d "G:\WYHQ-Studying\Code\web\html&css\01_the_first_web_page"
 
-REM 添加所有文件到 Git 暂存区
+REM 添加所有文件（包括子文件夹）到 Git 暂存区
 git add .
 
-REM 提交更改
+REM 提交更改（默认提交信息）
 git commit -m "Update site content"
 
-REM 推送到远程仓库的 master 分支
-git push origin master
+REM 推送本地 master 到远程 main 分支
+git push origin master:main
 
 pause
